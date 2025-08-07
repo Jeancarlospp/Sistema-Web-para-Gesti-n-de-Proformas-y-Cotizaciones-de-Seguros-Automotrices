57410f7fe0362cbf97d8b3640d3329a6645a9c53
Jeancarlo Javier Santi Cruz <sjeancarlo.05jjsc@gmail.com>
2025-08-06 19:26:34 -0500

    Se agrega la base de datos completa

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,7 +1,7 @@
 <?php
 class Conexion {
     private $host = "localhost";
-    private $db = "sistema_cotizaciones";
+    private $db = "sistemas_cotizaciones";
     private $user = "admin";
     private $pass = "admin";
     public $conn;
